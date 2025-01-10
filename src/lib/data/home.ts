b38a885f4f10cdbb9774d9d4c80dc8b804a6b045
f12-mou <faria12mou@gmail.com>
2025-01-10 21:02:50 +0600

    again

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -11,23 +11,31 @@ export const description =
 	'Hello! I’m a Level 4 Term II Computer Science and Engineering student at Bangladesh University of Engineering and Technology (BUET). My journey in computer science has been an incredible adventure filled with passion, learning, and growth. Along the way, I’ve actively participated in extracurricular activities such as competitive programming and hackathons. Each project and competition shown here reflects my enthusiasm for learning new things. Hope to learn more in future. Thanks for visiting my portfolio.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/f12-mou' },
+	{ platform: Platform.GitHub, link: 'https://github.com/' },
 	{
 		platform: Platform.Linkedin,
-		link: 'https://www.linkedin.com/in/faria-mou-26225b342/'
+		link: 'https://www.linkedin.com/'
+	},
+	{
+		platform: Platform.Twitter,
+		link: 'https://twitter.com/'
+	},
+	{
+		platform: Platform.StackOverflow,
+		link: 'https://stackoverflow.com/'
 	},
 	{
 		platform: Platform.Email,
-		link: 'fariaNOSPACE12NOSPACEmouATgoogleDOTcom'
+		link: 'riadh_adrani@hotmail.fr'
 	},
 	{
 		platform: Platform.Youtube,
-		link: 'https://www.youtube.com/@fariamou-d3e'
+		link: 'https://www.youtube.com'
 	},
 	{
 		platform: Platform.Facebook,
-		link: 'https://www.facebook.com/profile.php?id=100015497264049'
+		link: 'https://www.facebook.com'
 	}
 ];
 
-export const skills = getSkills('js', 'css', 'html', 'svelte', 'ts');
+export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts');
