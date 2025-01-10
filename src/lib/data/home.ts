7e0e13665328778b59e2de960fb59e02dce43ae0
f12-mou <faria12mou@gmail.com>
2025-01-10 20:56:07 +0600

    first page updated

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -8,34 +8,26 @@ export const name = 'Faria Binta';
 export const lastName = 'Awal';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'Hello! I’m a Level 4 Term II Computer Science and Engineering student at Bangladesh University of Engineering and Technology (BUET). My journey in computer science has been an incredible adventure filled with passion, learning, and growth. Along the way, I’ve actively participated in extracurricular activities such as competitive programming and hackathons. Each project and competition shown here reflects my enthusiasm for learning new things. Hope to learn more in future. Thanks for visiting my portfolio.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ platform: Platform.GitHub, link: 'https://github.com/f12-mou' },
 	{
 		platform: Platform.Linkedin,
-		link: 'https://www.linkedin.com/'
-	},
-	{
-		platform: Platform.Twitter,
-		link: 'https://twitter.com/'
-	},
-	{
-		platform: Platform.StackOverflow,
-		link: 'https://stackoverflow.com/'
+		link: 'https://www.linkedin.com/in/faria-mou-26225b342/'
 	},
 	{
 		platform: Platform.Email,
-		link: 'riadh_adrani@hotmail.fr'
+		link: 'fariaNOSPACE12NOSPACEmouATgoogleDOTcom'
 	},
 	{
 		platform: Platform.Youtube,
-		link: 'https://www.youtube.com'
+		link: 'https://www.youtube.com/@fariamou-d3e'
 	},
 	{
 		platform: Platform.Facebook,
-		link: 'https://www.facebook.com'
+		link: 'https://www.facebook.com/profile.php?id=100015497264049'
 	}
 ];
 
-export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts');
+export const skills = getSkills('js', 'css', 'html', 'svelte', 'ts');
