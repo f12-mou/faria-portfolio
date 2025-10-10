7dc05f0b605b2f779ecd43927e3dedddb8025cee
faria <faria12mou@gmail.com>
2025-10-11 02:33:58 +0600

    added cv

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,10 @@
-export const data = '';
+// export const data = '';
 
-export const title = 'Resumé';
+// export const title = 'Resumé';
+
+
+// src/data/resume.js
+export const title = "My Resume";
+
+// Path to the CV in the `static` folder
+export const data = "/Faria_CV.pdf"; 
