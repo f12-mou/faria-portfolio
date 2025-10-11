8aeef4de575a1d541cc58f99ed2710aeabdc9887
faria <faria12mou@gmail.com>
2025-10-12 02:35:34 +0600

    Carousels removed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,15 +42,4 @@
 		</div>
 	</div>
 	<!-- <Carrousel items={skills ?? skillsItems} /> -->
-
-	<div class="mt-10 w-full flex justify-center">
-		<Carrousel
-			images={['/images/snip1.png', '/images/snip2.png']}
-			autoplay={true}
-			interval={3000}
-			showIndicators={true}
-			showArrows={true}
-			class="max-w-3xl rounded-2xl shadow-lg"
-		/>
-	</div>
 </div>
