236975afee86c0bd55b81df7a0b1fd7bd1c131be
f12-mou <78227158+f12-mou@users.noreply.github.com>
2025-10-11 20:51:30 +0000

    Deploying to gh-pages from @ f12-mou/faria-portfolio@d933936099251c1a75610d233b41844360011a70 🚀

--- _app/immutable/chunks/stores.cffaf460.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.1f1d5b8c.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
