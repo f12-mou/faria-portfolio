d3f0fd1d5a27ebaa6a75b2f7569c6e216bacc6f5
faria <faria12mou@gmail.com>
2025-10-12 01:31:32 +0600

    Added the research projects

--- src/routes/projects/[slug]/+page.ts
+++ src/routes/projects/[slug]/+page.ts
@@ -1,11 +1,25 @@
-import { items } from '@data/projects';
+
+import { items, research } from '@data/projects';
 
 export function load({ params }: { params: Record<string, string> }) {
 	if (params.slug) {
-		const project = items.find((item) => {
-			return item.slug === params.slug;
-		});
+		// Try to find the project in both arrays
+		const project =
+			items.find((item) => item.slug === params.slug) ||
+			research.find((item) => item.slug === params.slug);
 
 		return { project };
 	}
 }
+
+// import { items } from '@data/projects';
+
+// export function load({ params }: { params: Record<string, string> }) {
+// 	if (params.slug) {
+// 		const project = items.find((item) => {
+// 			return item.slug === params.slug;
+// 		});
+
+// 		return { project };
+// 	}
+// }
