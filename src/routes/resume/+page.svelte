fe90009b99415818fe6182a9a62b610323c2b327
faria <faria12mou@gmail.com>
2025-10-12 02:41:23 +0600

    Resume fixed

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -24,7 +24,7 @@
 		<iframe
 			src="https://drive.google.com/file/d/1OAUnK5BKDAEIiJiDEetgcIvynLvYYa04/preview"
 			width="190%"
-			height="1800px"
+			height="5000px"
 		></iframe>
 	</div>
 </CommonPage>
